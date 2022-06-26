b5ec3a6e2b6d6cfd10b06bfcaf716ab2f370e5af
SanjayPaul4u <sanjoypaul655@gmail.com>
2022-06-26 22:34:56 +0530

    first commit : 74 video completed: Redux app set up

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2 className='pb-5'>Buy Addidas shoes 50 Rs</h2>
+        <button className="btn btn-primary mx-2">+</button>
+        Add to card
+        <button className="btn btn-primary mx-2">-</button>
+    </div>
+  )
+}
+
+export default Shop
